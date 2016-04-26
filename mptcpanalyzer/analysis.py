b58ec6cde370ff844bd988643ad373f165803e5d
Matthieu Coudron <mattator@gmail.com>
2016-04-27 00:17:57 +0200

    WIP: numerics

--- mptcpanalyzer/analysis.py
+++ mptcpanalyzer/analysis.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3.5
 # attempt to do some monkey patching
+# sympify can generate symbols from string
+# http://docs.sympy.org/dev/modules/core.html?highlight=subs#sympy.core.basic.Basic.subs
 from mptcpanalyzer.command import Command
 
 from enum import Enum, IntEnum
@@ -57,74 +59,85 @@ class DssMapping(IntEnum):
 def rto(rtt, var):
     return rtt + 4*var
 
-class MpTcpOverhead(Command):
+def dss_size(ack : DssAck, mapping : DssMapping, with_checksum: bool=False) -> int:
     """
-
     """
+    size = 4
+    size += ack.value
+    size += mapping.value
+    size += 2 if checksum else 0
+    return size
 
-    def __init__(self):
-        pass
+# class MpTcpOverhead(Command):
+#     """
 
-    def _dss_size(ack : DssAck, mapping : DssMapping, with_checksum: bool=False) -> int:
-        """
-        """
-        size = 4
-        size += ack.value
-        size += mapping.value
-        size += 2 if checksum else 0
-        return size
+#     """
 
-    def _overhead_const (total_nb_of_subflows : int):
-        """
-        Returns constant overhead for a connection
+#     def __init__(self):
+#         pass
 
-        Mp_CAPABLE + MP_DSSfinal + sum of MP_JOIN
-        """
-        oh_mpc, oh_finaldss, oh_mpjoin = sp.symbols("OH_{MP_CAPABLE} OH_{Final dss} OH_{MP_JOIN}")
-        # TODO test en remplacant les symboles
-        # TODO plot l'overhead d'une connexion
-        constant_oh = oh_mpc + oh_finaldss + sp.sum(oh_mpjoin, ())
-        return OptionSize.Capable.value + total_nb_of_subflows * OptionSize.Join.value
+#     def _dss_size(ack : DssAck, mapping : DssMapping, with_checksum: bool=False) -> int:
+#         """
+#         """
+#         size = 4
+#         size += ack.value
+#         size += mapping.value
+#         size += 2 if checksum else 0
+#         return size
 
-    def do(self, data):
-        parser = argparse.ArgumentParser(description="Plot overhead")
-        parser.add_argument("topologie", action="store", help="File to load topology from")
-        args = parser.parse_args(shlex.split(args))
-        # print("hello world")
-        # json.load()
-# TODO this should be a plot rather than a command
-        print("topology=", args.topology ) 
-        with open(args.topology) as f:
-            j = json.load(f)
-            print("Number of subflows=%d" % len(j["subflows"]))
-            for s in j["subflows"]:
-                print("MSS=%d" % s["mss"])
-# TODO sy.add varying overhead
-                # sy.add 
-            print("toto")
+#     def _overhead_const (total_nb_of_subflows : int):
+#         """
+#         Returns constant overhead for a connection
 
-    def help(self):
-        """
-        """
-        print("Allow to generate stats")
+#         Mp_CAPABLE + MP_DSSfinal + sum of MP_JOIN
+#         """
+#         oh_mpc, oh_finaldss, oh_mpjoin, nb_subflows = sp.symbols("OH_{MP_CAPABLE} OH_{Final dss} OH_{MP_JOIN} n")
+#         # TODO test en remplacant les symboles
+#         # TODO plot l'overhead d'une connexion
+#         constant_oh = oh_mpc + oh_finaldss + oh_mpjoin * nb_subflows
+#         # look at simpify
+#         # .subs(
+#         # todo provide a dict
+#         constant_oh.evalf()
+#         return OptionSize.Capable.value + total_nb_of_subflows * OptionSize.Join.value
 
-    def complete(self, text, line, begidx, endidx):
-        """
-        """
+#     def do(self, data):
+#         parser = argparse.ArgumentParser(description="Plot overhead")
+#         parser.add_argument("topologie", action="store", help="File to load topology from")
+#         args = parser.parse_args(shlex.split(args))
+#         # print("hello world")
+#         # json.load()
+# # TODO this should be a plot rather than a command
+#         print("topology=", args.topology ) 
+#         with open(args.topology) as f:
+#             j = json.load(f)
+#             print("Number of subflows=%d" % len(j["subflows"]))
+#             for s in j["subflows"]:
+#                 print("MSS=%d" % s["mss"])
+# # TODO sy.add varying overhead
+#                 # sy.add 
+#             print("toto")
 
-# name/value
+#     def help(self):
+#         """
+#         """
+#         print("Allow to generate stats")
+
+#     def complete(self, text, line, begidx, endidx):
+#         """
+#         """
+
+# # name/value
 class HOLTypes(Enum):
     """
     names inspired from  SCTP paper
 
     """
-    GapedAck = "GapedAck"
+    GapedAck = "GapAck-Induced Sender Buffer Blocking (GSB)"
     RcvBufferBlocking = "rcv buffer RcvBufferBlocking"
-
-   RWB = "Window-Induced Receiver Buffer Blocking"
-   ReceiverReorderingBlocking = "Reordering-Induced Receiver Buffer Blocking"
+    ReceiverWindowBlocking = "Window-Induced Receiver Buffer Blocking"
+    ReceiverReorderingBlocking = "Reordering-Induced Receiver Buffer Blocking"
 	Transmission-Induced Sender Buffer Blocking (TSB)
-	GapAck-Induced Sender Buffer Blocking (GSB)
 
 
 
@@ -179,7 +192,17 @@ class MpTcpNumerics(cmd.Cmd):
         # sp.lcm(rtt)
 
     def do_compute_constraints(self, args):
-        ds
+        """
+        Options and buffer size are loaded from topologies
+        """
+        # sp.symbols("
+        # out of order queue
+        rcv_ooo = []
+        rcv_left, rcv_wnd, rcv_max_wnd = sp.symbols("dsn_{rcv} w_{rcv} w^{max}_{rcv}")
+        # ds
+        nb_of_subflows = len(self.j["subflows"])
+        snd_left, snd_cwnd = sp.symbols("")
+        w = []
         return
 
 
@@ -188,6 +211,47 @@ class MpTcpNumerics(cmd.Cmd):
         Quit/exit program
         """
         return True
+
+    def _overhead_variable(self):
+        """
+        depends on MTU and DSS ratio
+        In RFC6991, it is explicit that MSS does not include the TCP header.
+The fixed size of the TCP header is of 20 bytes. The fixed size of the IPv4
+header is 20 bytes (40 for Ipv6).
+The classic/widely available MSS is 1500 bytes (without Ethernet) hence
+1500- 40 (IP + TCP) = 1460 is available. 536 octets is the minimum MSS. 1360
+        We suppose one ack per 
+
+        a dss mapping can cover at most 2^16 = 65536 bytes
+        """
+# TODO try to get the latex for mean
+        n_dack, n_dss, mss, total_bytes, dss_coverage  = sp.symbols("n_{dack} n_{dss} N_{packets} MSS N DSS_{coverage}")
+        nb_of_packets = total_bytes/mss
+        return n_dack* nb_of_packets + n_dss * total_bytes/dss_coverage
+
+    def _overhead(self):
+        """
+        """
+        return _overhead_const + _overead_variable():
+            # OH_{MP_CAPABLE} OH_{Final dss} OH_{MP_JOIN} n
+        # return OptionSize.Capable.value + total_nb_of_subflows * OptionSize.Join.value
+
+    def _overhead_const (self, total_nb_of_subflows : int):
+        """
+        Returns constant overhead for a connection
+
+        Mp_CAPABLE + MP_DSSfinal + sum of MP_JOIN
+        """
+        oh_mpc, oh_finaldss, oh_mpjoin, nb_subflows = sp.symbols("OH_{MP_CAPABLE} OH_{Final dss} OH_{MP_JOIN} n")
+        # TODO test en remplacant les symboles
+        # TODO plot l'overhead d'une connexion
+        constant_oh = oh_mpc + oh_finaldss + oh_mpjoin * nb_subflows
+        return constant_oh
+        # look at simpify
+        # .subs(
+        # todo provide a dict
+        # constant_oh.evalf()
+
 
 def run():
     parser = argparse.ArgumentParser(
