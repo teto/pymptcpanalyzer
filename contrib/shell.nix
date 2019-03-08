33f7001452ade9b0b97864f2f1ac915a5cfb4e4e
Matthieu Coudron <mattator@gmail.com>
2019-03-08 17:51:04 +0900

    fix pandas further

--- contrib/shell.nix
+++ contrib/shell.nix
@@ -1,7 +1,29 @@
 # { pkgs ? import <nixpkgs> {} }:
 with import <nixpkgs> {};
 let
-  prog = mptcpanalyzer.overridePythonAttrs (oa: {
+
+    pandas = python3Packages.pandas.overridePythonAttrs (oa: {
+
+      src = fetchFromGitHub {
+        owner = "teto";
+        repo = "pandas";
+        rev = "7072085b82ed30a79b612962dba3d17a60d2c352";
+        sha256 = "1i8mvgypcvbwdziqb3zw0z9cpnczz2k3814wqrb4v9a30rd6g66f";
+      };
+
+      # src = super.fetchFromGitHub {
+      #   owner = "pandas-dev";
+      #   repo = "pandas";
+      #   rev = "9c0f6a8d703b6bee48918f2c5d16418a7ff736e3";
+      #   sha256 = "0czdfn82sp2mnw46n90xkfvfk7r0zgfyhfk0npnglp1jpfndpj3i";
+      # };
+
+      doCheck = false;
+      installCheckPhase = false;
+    });
+
+  # TODO override pandas
+  prog = (mptcpanalyzer.override({ inherit pandas; }) ).overridePythonAttrs (oa: {
 
     propagatedBuildInputs = oa.propagatedBuildInputs ++ [
       # to publish on pypi
