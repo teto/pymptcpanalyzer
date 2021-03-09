8251d7127a7cbca2b824793386742c06fba38954
Matthieu Coudron <mcoudron@hotmail.com>
2021-03-09 03:33:54 +0100

    flake works: TODO check it can still plot

--- contrib/default.nix
+++ contrib/default.nix
@@ -6,9 +6,12 @@
 let
   mptcpanalyzer = poetry2nix.mkPoetryApplication {
     projectDir = ../.;
-    preferWheels = false;
+    # preferWheels = false;
     overrides = poetry2nix.overrides.withDefaults (self: super: {
 
+      pyqt5 = super.pyqt5.override { withMultimedia = true; };
+      # pyqt5-qt = self.pyqt5_with_qtmultimedia;
+      # pyqt5_qt = self.pyqt5_with_qtmultimedia;
       matplotlib = (super.matplotlib.override { enableGtk3=true;}).overrideAttrs(oa: {
         buildInputs = oa.buildInputs ++ [ pango ];
         strictDeps = false;
--- flake.nix
+++ flake.nix
@@ -3,15 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # poetry.url = "github:nix-community/poetry2nix";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: let
-    mptcpanalyzer = nixpkgs.lib.callPackage ./default.nix {};
-  in {
+  outputs = { self, nixpkgs, flake-utils }: let
+  in flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      mptcpanalyzer = pkgs.callPackage ./contrib/default.nix {};
+    in rec {
 
-    packages.x86_64-linux.mptcpanalyzer = mptcpanalyzer;
+    packages.mptcpanalyzer = mptcpanalyzer;
 
     devShell = mptcpanalyzer;
     defaultPackage = mptcpanalyzer;
-  };
+  });
 }
